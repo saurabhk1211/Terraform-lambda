810855d7402217a9ef4d3410dfa63f2a458a7c6a
saurabhk1211 <63963071+saurabhk1211@users.noreply.github.com>
2024-05-19 16:07:29 +0100

    Update hello-python.py

--- python/hello-python.py
+++ python/hello-python.py
@@ -1,3 +1,3 @@
 def lambda_handler(event, context):
-   return "This is the function I created using terraforgggggggggggggggggggggggggg"
+   return "This is the function I created using terraforhhhhhhhhhhhhhhhh"
    
